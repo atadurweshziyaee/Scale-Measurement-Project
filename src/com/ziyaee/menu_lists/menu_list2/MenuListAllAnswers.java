bc874cc1e7c098dd6ee56ddb501334578585a27b
Ata <atadurweshziyaee@gmail.com>
2024-02-28 03:40:59 +0530

    It will show the list of items or measurement scale.

---
+++ src/com/ziyaee/menu_lists/menu_list2/MenuListAllAnswers.java
@@ -0,0 +1,22 @@
+package com.ziyaee.menu_lists.menu_list2;
+
+public class MenuListAllAnswers {
+
+    public void menuListAllAnswers(){
+
+        System.out.println("\n\n\t\t\t\tXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        System.out.println("\t\t\t\tX\t\t\t 0)  goto previous menu.                          X");
+        System.out.println("\t\t\t\tX\t\t\t 1)  Kilometre to Something.                      X");
+        System.out.println("\t\t\t\tX\t\t\t 2)  Metre to Something.                          X");
+        System.out.println("\t\t\t\tX\t\t\t 3)  Centimetre to Something.                     X");
+        System.out.println("\t\t\t\tX\t\t\t 4)  Millimetre to Something.                     X");
+        System.out.println("\t\t\t\tX\t\t\t 5)  Micrometre to Something.                     X");
+        System.out.println("\t\t\t\tX\t\t\t 6)  Nanometre to Something.                      X");
+        System.out.println("\t\t\t\tX\t\t\t 7)  Mile to Something.                           X");
+        System.out.println("\t\t\t\tX\t\t\t 8)  Yard to Something.                           X");
+        System.out.println("\t\t\t\tX\t\t\t 9)  Foot to Something.                           X");
+        System.out.println("\t\t\t\tX\t\t\t10)  Inch to Something.                           X");
+        System.out.println("\t\t\t\tX\t\t\t11)  Nautical Mile to Something.                  X");
+        System.out.println("\t\t\t\tXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+    }
+}
