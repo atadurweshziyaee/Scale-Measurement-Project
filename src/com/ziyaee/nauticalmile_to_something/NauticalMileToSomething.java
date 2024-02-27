c9eede6f6fd6bd2bf6f6a3d04458d3e2ba530169
Ata <atadurweshziyaee@gmail.com>
2024-02-28 03:53:11 +0530

    This will convert Nautical Mile to anything.

---
+++ src/com/ziyaee/nauticalmile_to_something/NauticalMileToSomething.java
@@ -0,0 +1,46 @@
+package com.ziyaee.nauticalmile_to_something;
+
+public class NauticalMileToSomething {
+
+    public double nauticalMileToKilometre(double nauticalMile){
+        return nauticalMile * 1.852d;
+    }
+
+    public double nauticalMileToMetre(double nauticalMile){
+        return nauticalMile * 1852.0d;
+    }
+
+    public double nauticalMileToCentimetre(double nauticalMile){
+        return nauticalMile * 185200.0d;
+    }
+
+    public double nauticalMileToMillimetre(double nauticalMile){
+        return nauticalMile * 1852000.0d;
+    }
+
+    public double nauticalMileToMicrometre(double nauticalMile){
+        return nauticalMile * 1.852e9d;
+    }
+
+    public double nauticalMileToNanometre(double nauticalMile){
+        return nauticalMile * 1.852e12d;
+    }
+
+    public double nauticalMileToMile(double nauticalMile){
+        return nauticalMile * 1.15078d;
+    }
+
+    public double nauticalMileToYard(double nauticalMile){
+        return nauticalMile * 2025.37183d;
+    }
+
+    public double nauticalMileToFoot(double nauticalMile){
+        return nauticalMile * 6076.11549d;
+    }
+
+    public double nauticalMileToInch(double nauticalMile){
+        return nauticalMile * 72913.3858d;
+    }
+
+
+}
