93538d04e713b9ef2e9fe1c2f3ffb52f35fa9c26
Ata <atadurweshziyaee@gmail.com>
2024-02-28 03:44:16 +0530

    It will convert Meter to something by user wants

---
+++ src/com/ziyaee/micrometre_to_something/MicrometreToSomething.java
@@ -0,0 +1,56 @@
+package com.ziyaee.micrometre_to_something;
+
+public class MicrometreToSomething {
+
+    // converting (Micrometre to Kilometre).
+    public double micrometreToKilometre(double micrometre) {
+        return micrometre / 1000000000.0d;
+    }
+
+    // converting (Micrometre to Metre).
+    public double micrometreToMetre(double micrometre) {
+        return micrometre / 1000000.0d;
+    }
+
+    // converting (Micrometre to Centimetre).
+    public double micrometreToCentimetre(double micrometre) {
+        return micrometre / 10000.0d;
+    }
+
+    // converting (Micrometre to Millimetre).
+    public double micrometreToMillimetre(double micrometre) {
+        return micrometre / 1000.0d;
+    }
+
+    // converting (Micrometre to Nanometre).
+    public double micrometreToNanometre(double micrometre) {
+        return micrometre * 1000.0d;
+    }
+
+    // converting (Micrometre to Mile).
+    public double micrometreToMile(double micrometre) {
+        return micrometre / 1609344000.0d;
+    }
+
+    // converting (Micrometre to Yard).
+    public double micrometreToYard(double micrometre) {
+        return micrometre / 914400.0d;
+    }
+
+    // converting (Micrometre to Foot).
+    public double micrometreToFoot(double micrometre) {
+        return micrometre / 304800.0d;
+    }
+
+    // converting (Micrometre to Inch).
+    public double micrometreToInch(double micrometre) {
+        return micrometre / 25400.0d;
+    }
+
+    // converting (Micrometre to Nautical Mile).
+    public double micrometreToNauticalMile(double micrometre) {
+        return micrometre / 1852000000.0d;
+    }
+
+
+}
