374848bc2f31c4038db6b4c54a2f0249c56f1957
Ata <atadurweshziyaee@gmail.com>
2024-02-28 03:39:39 +0530

    It will show the Options to convert scales

---
+++ src/com/ziyaee/menu_lists/menu_list1/MenuList.java
@@ -0,0 +1,55 @@
+package com.ziyaee.menu_lists.menu_list1;
+
+public class MenuList {
+    public void menuList(){
+        System.out.println("\n");
+        System.out.println("                         \t\t\t-2)  -->ALL ANSWERS<--            \t\t\t 0)  exit.\n");
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        System.out.println("X\t\t\t 1)  Kilometre to Metre.          \t\t\t11)  Metre to Kilometre.        \t\t\t21)  Centimetre to Kilometre.    X");
+        System.out.println("X\t\t\t 2)  Kilometre to Centimetre.     \t\t\t12)  Metre to Centimetre.       \t\t\t22)  Centimetre to Metre.        X");
+        System.out.println("X\t\t\t 3)  Kilometre to Millimetre.     \t\t\t13)  Metre to Millimetre.       \t\t\t23)  Centimetre to Millimetre.   X");
+        System.out.println("X\t\t\t 4)  Kilometre to Micrometre.     \t\t\t14)  Metre to Micrometre.       \t\t\t24)  Centimetre to Micrometre.   X");
+        System.out.println("X\t\t\t 5)  Kilometre to Nanometre.      \t\t\t15)  Metre to Nanometre.        \t\t\t25)  Centimetre to Nanometre.    X");
+        System.out.println("X\t\t\t 6)  Kilometre to Mile.           \t\t\t16)  Metre to Mile.             \t\t\t26)  Centimetre to Mile.         X");
+        System.out.println("X\t\t\t 7)  Kilometre to Yard.           \t\t\t17)  Metre to Yard.             \t\t\t27)  Centimetre to Yard.         X");
+        System.out.println("X\t\t\t 8)  Kilometre to Foot.           \t\t\t18)  Metre to Foot.             \t\t\t28)  Centimetre to Foot.         X");
+        System.out.println("X\t\t\t 9)  Kilometre to Inch.           \t\t\t19)  Metre to Inch.             \t\t\t29)  Centimetre to Inch.         X");
+        System.out.println("X\t\t\t10)  Kilometre to Nautical Mile.  \t\t\t20)  Metre to Nautical Mile.    \t\t\t30)  Centimetre to Nautical Mile.X\n");
+
+        System.out.println("X\t\t\t31)  Millimetre to Kilometre.     \t\t\t41)  Micrometre to Kilometre.   \t\t\t51)  Nanometre to Kilometre.     X");
+        System.out.println("X\t\t\t32)  Millimetre to Metre.         \t\t\t42)  Micrometre to Metre.       \t\t\t52)  Nanometre to Metre.         X");
+        System.out.println("X\t\t\t33)  Millimetre to Centimetre.    \t\t\t43)  Micrometre to Centimetre.  \t\t\t53)  Nanometre to Centimetre.    X");
+        System.out.println("X\t\t\t34)  Millimetre to Micrometre.    \t\t\t44)  Micrometre to Millimetre.  \t\t\t54)  Nanometre to Millimetre.    X");
+        System.out.println("X\t\t\t35)  Millimetre to Nanometre.     \t\t\t45)  Micrometre to Nanometre.   \t\t\t55)  Nanometre to Micrometre.    X");
+        System.out.println("X\t\t\t36)  Millimetre to Mile.          \t\t\t46)  Micrometre to Mile.        \t\t\t56)  Nanometre to Mile.          X");
+        System.out.println("X\t\t\t37)  Millimetre to Yard.          \t\t\t47)  Micrometre to yard.        \t\t\t57)  Nanometre to Yard.          X");
+        System.out.println("X\t\t\t38)  Millimetre to Foot.          \t\t\t48)  Micrometre to Foot.        \t\t\t58)  Nanometre to Foot.          X");
+        System.out.println("X\t\t\t39)  Millimetre to Inch.          \t\t\t49)  Micrometre to Inch.        \t\t\t59)  Nanometre to Inch.          X");
+        System.out.println("X\t\t\t40)  Millimetre to Nautical Mile. \t\t\t50)  Micrometre to Nautical Mile.\t\t\t60)  Nanometre to Nautical Mile. X\n");
+
+        System.out.println("X\t\t\t61)  Mile to Kilometre.           \t\t\t71)  Yard to Kilometre.         \t\t\t81)  Foot to Kilometre.          X");
+        System.out.println("X\t\t\t62)  Mile to Metre.               \t\t\t72)  Yard to Metre.             \t\t\t82)  Foot to Metre.              X");
+        System.out.println("X\t\t\t63)  Mile to Centimetre.          \t\t\t73)  Yard to Centimetre.        \t\t\t83)  Foot to Centimetre.         X");
+        System.out.println("X\t\t\t64)  Mile to Millimetre.          \t\t\t74)  Yard to Millimetre.        \t\t\t84)  Foot to Millimetre.         X");
+        System.out.println("X\t\t\t65)  Mile to Micrometre.          \t\t\t75)  Yard to Micrometre.        \t\t\t85)  Foot to Micrometre.         X");
+        System.out.println("X\t\t\t66)  Mile to Nanometre.           \t\t\t76)  Yard to Nanometre.         \t\t\t86)  Foot to Nanometre.          X");
+        System.out.println("X\t\t\t67)  Mile to Yard.                \t\t\t77)  Yard to Mile.              \t\t\t87)  Foot to Mile.               X");
+        System.out.println("X\t\t\t68)  Mile to Foot.                \t\t\t78)  Yard to Foot.              \t\t\t88)  Foot to yard.               X");
+        System.out.println("X\t\t\t69)  Mile to Inch.                \t\t\t79)  Yard to Inch.              \t\t\t89)  Foot to Inch.               X");
+        System.out.println("X\t\t\t70)  Mile to Nautical Mile.       \t\t\t80)  Yard to Nautical Mile.     \t\t\t90)  Foot to Nautical Mile.      X\n");
+
+        System.out.println("X\t\t\t91)   Inch to Kilometre.         \t\t\t101)  Nautical Mile to Kilometre.                                            X");
+        System.out.println("X\t\t\t92)   Inch to Metre.             \t\t\t102)  Nautical Mile to Metre.                                                X");
+        System.out.println("X\t\t\t93)   Inch to Centimetre.        \t\t\t103)  Nautical Mile to Centimetre.                                           X");
+        System.out.println("X\t\t\t94)   Inch to Millimetre.        \t\t\t104)  Nautical Mile to Millimetre.                                           X");
+        System.out.println("X\t\t\t95)   Inch to Micrometre.        \t\t\t105)  Nautical Mile to Micrometre.                                           X");
+        System.out.println("X\t\t\t96)   Inch to Nanometre.         \t\t\t106)  Nautical Mile to Nanometre.                                            X");
+        System.out.println("X\t\t\t97)   Inch to Mile.              \t\t\t107)  Nautical Mile to Mile.                                                 X");
+        System.out.println("X\t\t\t98)   Inch to Yard.              \t\t\t108)  Nautical Mile to Yard.                                                 X");
+        System.out.println("X\t\t\t99)   Inch to Foot.              \t\t\t109)  Nautical Mile to Foot.                                                 X");
+        System.out.println("X\t\t\t100)  Inch to Nautical Mile.     \t\t\t110)  Nautical Mile to Inch.                                                 X");
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+
+    }
+}
