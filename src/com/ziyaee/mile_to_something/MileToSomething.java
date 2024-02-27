51f985905d401991936d4b111c9c87745edb9f65
Ata <atadurweshziyaee@gmail.com>
2024-02-28 03:46:49 +0530

    It will comver the Mile to something by user wants

---
+++ src/com/ziyaee/mile_to_something/MileToSomething.java
@@ -0,0 +1,55 @@
+package com.ziyaee.mile_to_something;
+
+public class MileToSomething {
+
+    // converting (Mile to Kilometre).
+    public double mileToKilometre(double mile){
+        return mile * 1.60934d;
+    }
+
+    // converting (Mile to Metre).
+    public double mileToMetre(double mile){
+        return mile * 1609.34d;
+    }
+
+    // converting (Mile to Centimetre).
+    public double mileToCentimetre(double mile){
+        return mile * 160934.4d;
+    }
+
+    // converting (Mile to Millimetre).
+    public double mileToMillimetre(double mile){
+        return mile * 1609344d;
+    }
+
+    // converting (Mile to Micrometre).
+    public double mileToMicrometre(double mile){
+        return mile * 1_609_344_000.0d;
+    }
+
+    // converting (Mile to Nanometre).
+    public double mileToNanometre(double mile){
+        return mile * 1_609_344_000_000.0d;
+    }
+
+    // converting (Mile to Yard).
+    public double mileToYard(double mile){
+        return mile * 1760.0d;
+    }
+
+    // converting (Mile to Foot).
+    public double mileToFoot(double mile){
+        return mile * 5280.0d;
+    }
+
+    // converting (Mile to Inch).
+    public double mileToInch(double mile){
+        return mile * 63360.0;
+    }
+
+    // converting (Mile to Nautical Mile).
+    public double mileToNauticalMile(double mile){
+        return mile * 0.868976d;
+    }
+
+}
