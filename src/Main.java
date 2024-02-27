458a49d0af78aea6e2490d241d89dcdbfe14e811
Ata <atadurweshziyaee@gmail.com>
2024-02-28 03:19:51 +0530

    Main.java file for running the program.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+// this is main class for scale measurement.
+
+// These are user defined classes.
+
+import com.ziyaee.options.Options;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // this is the main method.
+
+        Options op = new Options();
+        op.options();
+
+
+    }
+}
